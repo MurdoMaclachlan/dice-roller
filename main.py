964f86dc6436ba01b005df5be551833bbec4eb96
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-02-03 14:58:43 +0000

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,10 @@ import libdr
 def mainMenu():
     options = [libdr.freeRoll, libdr.doAbilityScores]
     x = True
-    yArray = ["1", "2"]
+    optNums = ["1", "2"]
     while x == True:
-        y = 0
-        while y not in yArray:
-            y = str(input("Do you want to do free roll, or generate ability scores? [1/2]: "))
-            if y not in yArray:
-                print("\nPlease enter either 1 or 2.\n")
+        y = libdr.optValidate("Do you want to do free roll, or generate ability sores? [1, 2]: ",optNums)
+        print("")
         x = options[int(y)-1]()
 
 mainMenu()
