4cae10817b6337d3f7462b4dd2d8f24a63a508c7
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-02-03 14:49:33 +0000

    Update __init__.py

--- libdr/__init__.py
+++ libdr/__init__.py
@@ -1,2 +1,9 @@
+#rolls
 from .rolls import doAbilityScores
 from .rolls import freeRoll
+
+#validation
+from .validate import numTypeSubValidate
+from .validate import numTypeValidate
+from .validate import optValidate
+from .validate import subValidate
