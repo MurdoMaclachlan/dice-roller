a58dcc8d3001dc8099d8dbc777332fb9f950faef
Murdo Maclachlan <murdomaclachlan@gmail.com>
2020-02-04 00:31:59 +0000

    Update validate.py

--- libdr/validate.py
+++ libdr/validate.py
@@ -1,11 +1,11 @@
 #option validation
-def optValidate(prompt,yArray):
-  y = 0
-  while y not in yArray:
-    y = str(input(prompt))
-    if y not in yArray:
-      print("\nPlease enter one of the available options.\n")
-  return y
+def optValidate(prompt, yArray):
+    y = 0
+    while y not in yArray:
+        y = str(input(prompt))
+        if y not in yArray:
+            print("\nPlease enter one of the available options.\n")
+    return y
 #option validation
 
 #variable type validation
@@ -14,11 +14,11 @@ def numTypeSubValidate(x,extender,y):
   return numTypeValidate(x,extender,y)
 
 def numTypeValidate(x,extender,y):
-  if any(c.isalpha() for c in x) == y:
-    x = numTypeSubValidate(x,extender,y)
-  return x
+    if not any(c.isdigit() for c in x) == y:
+        x = numTypeSubValidate(x,extender,y)
+    return x
 
 def subValidate(extender):
-  x = input("Please enter " + extender)
+  x = input("\nPlease enter " + extender)
   return x
 #variable type validation
